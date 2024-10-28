fdae111d0ad84c3b0300addac433282d46e74830
Alexsmartthings06GM <alejandrogm06147@gmail.com>
2024-10-28 20:28:27 +0100

    corregido error de segundos

--- Juego-Clicker/js/codigo.js
+++ Juego-Clicker/js/codigo.js
@@ -3,24 +3,36 @@ var contador = 0;
 var segundoInicio = 10;  // Valor inicial de la cuenta regresiva
 var puedeHacerClick = true;  // Variable para habilitar o deshabilitar clics
 
+// CUENTA REGRESIVA
+function actualizar() {
+    document.getElementById('countdown').innerHTML = segundoInicio;
+
+    if (segundoInicio === 0) {
+        // Cuenta regresiva ha finalizado
+        document.getElementById('countdown').innerHTML = "Contador finalizado";
+        puedeHacerClick = false;  // Deshabilito los clics cuando termina la cuenta regresiva
+        alert("El juego ha terminado, hiciste " + contador + " clicks.");
+        resetearContador();  // Llama a la función correctamente
+    } else {
+        segundoInicio -= 1;
+        setTimeout(actualizar, 1000);
+    }
+}
+
 function incrementarContador() {
     // Solo incrementa el contador si puedeHacerClick es verdadero
     if (puedeHacerClick) {
         contador++;
-
-        // Actualizo los puntos (apunta al css) y muestro el botonReset
         document.getElementById("puntos").innerHTML = contador;
         document.getElementById("botonReset").style.display = "block";
     }
-
 }
 
 function resetearContador() {
     // Pongo el contador a 0
     contador = 0;
 
-    // Reinicio la cuenta regresiva y habilito los clics nuevamente, después restablezco el valor inicial, luego
-    //permito nuevamente los clicks y por último reinicio la cuenta regresiva.
+    // Reinicio la cuenta regresiva y habilito los clics nuevamente, después restablezco el valor inicial
     segundoInicio = 10; 
     puedeHacerClick = true;  
     actualizar();  
@@ -30,24 +42,8 @@ function resetearContador() {
     document.getElementById("botonReset").style.display = "none";
 }
 
-// CUENTA REGRESIVA
-function actualizar() {
-    document.getElementById('countdown').innerHTML = segundoInicio;
-
-    if (segundoInicio === 0) {
-        // Cuenta regresiva ha finalizado
-        document.getElementById('countdown').innerHTML = "Contador finalizado";
-        puedeHacerClick = false;  // Deshabilito los clics cuando termina la cuenta regresiva
-    } else {
-        segundoInicio -= 1;
-        setTimeout(actualizar, 1000);
-    }
-    if (segundoInicio <= 0){
-        alert("El juego ha terminado, hiciste " + contador + " clicks.");
-        resetearContador;
-    }
-}
 // Inicia la cuenta regresiva la primera vez
 actualizar();
+
 
 
