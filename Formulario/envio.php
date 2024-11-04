b60701e132dc01bbf5bf033993639203c5cd905e
alexsmartthings <alejandrogm06147@gmail.com>
2024-11-04 18:08:35 +0100

    formulario

---
+++ Formulario/envio.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+Hola <?php echo $_POST["nombre"]; ?> !<br>
+
+Tu correo es <?php echo $_POST["correo"]; ?>
+
+Has enviado el siguiente mensaje: <p> <?php echo $_POST["mensaje"]; ?> </p>
+
+Tu respuesta a si deseas recibir respuesta:  <?php echo $_POST["envioEmail"]; ?> 
+
+Tu elección sobre tu profesión: <?echo $_POST["profesion"]; ?>
+</body>
+</html>
